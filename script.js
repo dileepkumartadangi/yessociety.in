8594520e4ba53198d20ff0b4c1ee04c47469ba3c
dileepkumartadangi <151521329+dileepkumartadangi@users.noreply.github.com>
2026-03-15 09:47:01 +0530

    yessociety.in

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ e.preventDefault();
 alert("Message Sent Successfully!");
 
 });
-/* mobile comfort sample*/
+/* mobile comfort sample
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('nav ul');
+const menu = document.querySelector('.nav-menu');
 
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+/* mobile comfort sample*/
+
+const navToggle = document.querySelector('.nav-toggle');
+const mobileMenu = document.querySelector('.nav-menu');
+
+if(navToggle && mobileMenu){
+  navToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active'); // toggle menu only
+  });
+}
